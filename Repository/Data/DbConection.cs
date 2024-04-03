0ebdac130517118eb95fdcb85859d9020517bde0
mcsnoguera07 <134111806+mcsnoguera07@users.noreply.github.com>
2024-04-02 21:42:50 -0400

    Add files via upload

---
+++ Repository/Data/DbConection.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Dapper;
+using Npgsql;
+
+namespace Repository.Data
+{
+    public class IDbConection
+    {
+        private string connectionString; 
+        public  IDbConection(string _connectionString) 
+        {
+            this.connectionString = _connectionString;
+        }
+        public IDbConnection dbConnection()
+
+        {
+            try
+            {
+                IDbConnection conexion = new NpgsqlConnection(connectionString);
+                conexion.Open();
+                return conexion;
+
+            }
+            catch (Exception ex)
+            {
+                throw new NpgsqlException(ex.Message);
+            }
+            
+        }
+    }
+}
---
+++ Repository/Data/PersonaRepository.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Dapper;
+
+namespace Repository.Data
+{
+    internal class PersonaRepository : IPersona
+    {
+        private IDbConnection conexionDB;
+        public PersonaRepository(string _connectionString)
+        {
+            conexionDB = new IDbConection(_connectionString).dbConnection();
+        }
+        public bool add(PersonaModel persona)
+        {
+            try
+            {
+                if (conexionDB.Execute("insert into Persona(nombre, apellido, cedula) values(@nombre, @apellido, @cedula)", persona) > 0)
+                return true;
+                else
+                    return false;
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+        }
+
+        public  PersonaModel get(int id)
+        {
+            try
+            {
+                return conexionDB.QueryFirst<PersonaModel>($"SELECT * FROM Persona WHERE id = {id}");
+
+            }
+            catch (Exception ex)
+            {
+
+                throw new Exception(ex.Message);
+            }
+        }
+
+        public IEnumerable<PersonaModel> list()
+        {
+            try
+            {
+                return conexionDB.Query<PersonaModel>($"SELECT * FROM Persona order by id asc ");
+            }
+            catch (Exception ex)
+            {
+
+                throw new Exception(ex.Message);
+            }
+        }
+
+        public string remove(int id)
+        {
+            try
+            {
+                conexionDB.Execute($"DELETE FROM Persona WHERE id = {id} ");
+                return " Los datos fueron eliminados exitosamente...";
+            }
+            catch (Exception ex)
+            {
+
+                throw new Exception(ex.Message);
+            }
+        }
+
+        public string update(PersonaModel persona, int id)
+        {
+            try
+            {
+                conexionDB.Execute($"UPDATE Persona SET " +
+                    "nombre = @nombre, " +
+                    "apellido = @apellido, " +
+                    "cedula = @cedula, " +
+                    $"WHERE id = {id}", persona);
+                return " Los datos fueron modificados exitosamente...";
+
+            }
+            catch (Exception ex)
+            {
+
+                throw new Exception(ex.Message);
+            }
+        }
+    }
+}
